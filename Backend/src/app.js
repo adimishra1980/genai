76e43b5c85feae3f9aa63f267a511bf6a8191a0b
Aditya Mishra <adityamishra19880@gmail.com>
2026-08-25 00:42:04 +0530

    fix: dotenv config in backend app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,19 +1,23 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-import cors from "cors"
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+import authRouter from "./routes/auth.routes.js";
+
 const app = express();
 
-// middleware to configure json and url
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+  }),
+);
+
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    credentials: true,
-}))
-
-// import routes
-import authRouter from "./routes/auth.routes.js";
 
 // api routes
 app.use("/api/v1/auth", authRouter);
