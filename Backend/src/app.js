52c3d123717e53c9d3d56807f74c03e73c1b5b41
Aditya Mishra <adityamishra19880@gmail.com>
2026-08-24 23:10:24 +0530

    feat: add cors in the backend

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,12 +1,16 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-
+import cors from "cors"
 const app = express();
 
 // middleware to configure json and url
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+}))
 
 // import routes
 import authRouter from "./routes/auth.routes.js";
